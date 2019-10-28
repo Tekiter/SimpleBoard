9a6a639b6032120a134a59dfcc647b64b43489d7
Tekiter <tekiter8@gmail.com>
2019-10-28 20:04:22 +0900

    Create login structure base

--- server/app/api/v1/index.js
+++ server/app/api/v1/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const loginController = require('./login')
+const userController = require('./user')
 
-router.post('/login', loginController.login)
+router.post('/session', loginController.login)
+router.delete('/session', loginController.logout)
+
+router.post('/user', userController.register)
 
 module.exports = router
