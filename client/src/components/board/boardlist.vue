647a2cdf2404afce1be953cd318d5e993498f010
Tekiter <tekiter8@gmail.com>
2019-11-04 02:30:35 +0900

    Create board list

--- client/src/components/board/boardlist.vue
+++ client/src/components/board/boardlist.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        A Board List
+        <b-list-group>
+            <b-list-group-item v-for="board in boards" :key="board._id" :to="{path: `/${board._id}`}">
+                {{ board.name }}
+            </b-list-group-item>
+        </b-list-group>
     </div>
 </template>
 <style scoped>
@@ -8,6 +12,23 @@
 </style>
 <script>
 export default {
-    
+    data() {
+        return {
+            boards: []
+        }
+    },
+    methods: {
+        async updateBoardList() {
+            try {
+                const res = await this.$http.get('/board')
+                this.boards = res.data.boards
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    },
+    mounted() {
+        this.updateBoardList()
+    }
 }
 </script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/:board_id',
-    name: 'home',
+    name: 'board',
     component: () => import('../views/Home.vue'),
     children: [
       {
