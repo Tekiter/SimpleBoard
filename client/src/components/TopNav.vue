8f4724266588198ee2147c63900817c1ab57aceb
Tekiter <tekiter8@gmail.com>
2019-11-06 16:47:35 +0900

    Add navbar

---
+++ client/src/components/TopNav.vue
@@ -0,0 +1,43 @@
+<template>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand :to="{path: '/'}">Simple Board</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+
+      <b-navbar-nav class="ml-auto">
+        
+        <b-nav-item-dropdown right v-if="isLoggedIn">
+          <template v-slot:button-content>
+            <em>{{ username }}</em>
+          </template>
+          <b-dropdown-item @click="logout">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-navbar-nav v-else>
+            <b-nav-item @click="$router.push({path: '/login'})">Login</b-nav-item>
+        </b-navbar-nav>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+    computed: {
+        isLoggedIn() {
+            return this.$store.getters.isLoggedIn
+        },
+        username() {
+            return this.$store.state.session.username
+        }
+    },
+    methods: {
+        logout() {
+            this.$store.dispatch('logout')
+        }
+    }
+}
+</script>
--- client/src/store/session.js
+++ client/src/store/session.js
@@ -4,12 +4,20 @@ import session from '@/utils/session'
 export default {
     state: {
         accessToken: null,
+        username: null,
+        permission_level: 0
 
     },
     mutations: {
         
         SET_ACCESS_TOKEN(state, token) {
             state.accessToken = token
+        },
+        SET_USERNAME(state, username) {
+            state.username = username
+        },
+        SET_PERMISSION_LEVEL(state, permlevel) {
+            state.permission_level = permlevel
         }
     },
     actions: {
@@ -18,23 +26,20 @@ export default {
             context.commit('SET_ACCESS_TOKEN', accessToken)
         },
         async login(context, {username, password}) {
-            try {
+            
+            const res = await axios.post('/login', { username: username, password: password}, {
+                baseURL: session.apiURL
+            })
 
-                const res = await axios.post('/login', { username: username, password: password}, {
-                    baseURL: session.apiURL
-                })
-    
-                if (res.data.access_token) {
-                    localStorage.setItem('access_token', res.data.access_token)
-                    axios.defaults.headers.common['Authorization'] = `JWT ${res.data.access_token}`
-                    context.commit('SET_ACCESS_TOKEN', res.data.access_token)
-                    return
-                }
-                throw res
+            if (res.data.access_token) {
+                localStorage.setItem('access_token', res.data.access_token)
+                axios.defaults.headers.common['Authorization'] = `JWT ${res.data.access_token}`
+                context.commit('SET_ACCESS_TOKEN', res.data.access_token)
+                context.commit('SET_USERNAME', res.data.username)
+                context.commit('SET_PERMISSION_LEVEL', res.data.permission_level)
+                return
             }
-            catch (err) {
-                throw err
-            }
+            throw res
         },
         restoreLogin(context) {
             const token = localStorage.getItem('access_token')
@@ -47,6 +52,8 @@ export default {
             localStorage.removeItem('accessToken')
             delete axios.defaults.headers.common['Authorization']
             context.commit('SET_ACCESS_TOKEN', null)
+            context.commit('SET_USERNAME', null)
+            context.commit('SET_PERMISSION_LEVEL', 0)
         }
     },
     getters: {
